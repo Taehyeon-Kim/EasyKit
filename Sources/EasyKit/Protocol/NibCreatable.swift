24e4fb983fddbd6a669f4736fbc78e9fd42cca6c
김태현 <taehyk528@gmail.com>
2022-01-05 01:19:41 +0900

    Chore: NibCreatable Protocol Extension - nib, nibBundle, nibName 중복 요소 묶음

--- Sources/EasyKit/Protocol/NibCreatable.swift
+++ Sources/EasyKit/Protocol/NibCreatable.swift
@@ -6,3 +6,21 @@ public protocol NibCreatable {
     static var nibName: String { get }
     static func instanceFromNib() -> Self
 }
+
+public extension NibCreatable {
+    public final class var nib: UINib {
+        let nibName = Self.nibName
+        let nibBundle = Self.nibBundle
+        return UINib(nibName: nibName, bundle: nibBundle)
+    }
+    
+    @objc
+    public class var nibBundle: Bundle? {
+        return Bundle(for: self)
+    }
+    
+    @objc
+    public class var nibName: String {
+        return "\(self)"
+    }
+}
